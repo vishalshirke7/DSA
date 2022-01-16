451641e934ce81b202e74d4e7b1edf0f631a24f3
Vishal Shirke <vishalshirke17@gmail.com>
2022-01-16 12:04:57 +0530

    Re-arranged problems by categories

--- binary_search/capacity-to-ship-packages-within-d-days.py
+++ binary_search/capacity-to-ship-packages-within-d-days.py
@@ -21,4 +21,32 @@ def shipWithinDays(weights, D):
         return low
 
 
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:        
+        def can_ship(capacity):
+            total_days = days
+            total_wt = 0
+            for wt in weights:
+                if wt > capacity:
+                    return False
+                if wt + total_wt > capacity:
+                    total_wt = wt
+                    total_days -= 1
+                else:
+                    total_wt += wt
+                if total_days <= 0:
+                    return False
+            return True
+        
+        ans = float('inf')
+        start, end = 0, sum(weights)
+        while start <= end:
+            mid = (start + end) // 2
+            if can_ship(mid):
+                ans = min(ans, mid)
+                end = mid - 1
+            else:
+                start = mid + 1
+        return ans
+        
 print('Output', shipWithinDays([1,2,3,4,5,6,7,8,9,10], 5))        
--- test.py
+++ test.py
@@ -1,24 +1,44 @@
-# arr = [3,6,6,7,8]
-# size = len(arr)
+# # arr = [3,6,6,7,8]
+# # size = len(arr)
 
-# def reverse(start, arr):
+# # def reverse(start, arr):
+# #     if start >= size - start - 1:
+# #         return 
+# #     arr[start], arr[size - start - 1] = arr[size - start - 1], arr[start]
+# #     reverse(start + 1, arr)
+
+# # reverse(0, arr)
+# # print(arr)
+
+
+# input_str = 'madam'
+# size = len(input_str)
+
+# def check_palindrome(start, input_str):
 #     if start >= size - start - 1:
-#         return 
-#     arr[start], arr[size - start - 1] = arr[size - start - 1], arr[start]
-#     reverse(start + 1, arr)
+#         return True
+#     if input_str[start] != input_str[size - start - 1]:
+#         return False
+#     return check_palindrome(start + 1, input_str)
 
-# reverse(0, arr)
-# print(arr)
+# (print(check_palindrome(0, input_str)))
 
 
-input_str = 'madam'
-size = len(input_str)
 
-def check_palindrome(start, input_str):
-    if start >= size - start - 1:
-        return True
-    if input_str[start] != input_str[size - start - 1]:
-        return False
-    return check_palindrome(start + 1, input_str)
+def fibonacci_series(n):
+    a, b = 0, 1
+    if n == 0:
+        print(a)
+        return 
+    if n == 1:
+        print(b)
+        return
+    print(a)
+    print(b)  
+    for index in range(n - 2):
+        c = a + b
+        a = b
+        b = c
+        print(c)  
 
-(print(check_palindrome(0, input_str)))
+fibonacci_series(10) 
